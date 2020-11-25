e2d989c1f24dfaa3f6a8a53191e9a8246166beda
Rashad Walcott <rashad.walcott15@gmail.com>
2020-11-25 16:13:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //First Ex.
 function forLoop(array){
 for(let i=0; i<25;i++){
@@ -7,10 +8,21 @@ for(let i=0; i<25;i++){
   else
   {
     array.push(`I am ${i} strange loops.`);
+=======
+function forLoop(array){
+for(let i=0; i<25;i++){
+  if (i===1){
+    array[1] ="I am 1 strange loop.";
+  }
+  else
+  {
+    array[i]="I am " + [i] + "strange loops.";
+>>>>>>> a92efca794c115e162ca9f838ad1edf1dc956290
   }
 }
 return array;
 }
+<<<<<<< HEAD
 //Second Ex.
 function whileLoop(n){
   while (n >0){
@@ -30,3 +42,12 @@ var i =0;
   }
   while(incrementVariable() < num);
   }
+=======
+
+function whileLoop(num=15){
+  while (num >0){
+    console.log(--num);
+  }
+  return console.log("Done");
+}
+>>>>>>> a92efca794c115e162ca9f838ad1edf1dc956290
